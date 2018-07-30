910ccac49a212cb4424ed2746a6b9c16eb614e87
bp <bharat.balla@jifflenow.com>
2018-07-30 16:00:23 +0530

    Functionality to add Tasks

--- Todo.elm
+++ Todo.elm
@@ -1,6 +1,8 @@
 module Todo exposing (..)
 
-import Html exposing (Html, beginnerProgram, div, text, ul, li)
+import Html exposing (Html, beginnerProgram, div, text, ul, li, input, button)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput, onClick)
 
 
 type alias Task =
@@ -14,11 +16,14 @@ type alias Tasks =
 
 
 type alias Model =
-    Tasks
+    { tasks : Tasks
+    , newInput : String
+    }
 
 
 type Msg
     = Add
+    | TemporaryInput String
     | Delete
     | Done
     | Todo
@@ -27,7 +32,9 @@ type Msg
 main : Program Never Model Msg
 main =
     beginnerProgram
-        { model = [ Task "A" True, Task "AB" True, Task "ABC" True, Task "ABCD" True, Task "ABCDE" True ]
+        { model = Model [ Task "A" True, Task "AB" True, Task "ABC" True, Task "ABCD" True, Task "ABCDE" True ] ""
+
+        -- { model = []
         , view = view
         , update = update
         }
@@ -35,7 +42,21 @@ main =
 
 update : Msg -> Model -> Model
 update msg model =
-    model
+    case msg of
+        TemporaryInput task ->
+            { model | newInput = task }
+
+        Add ->
+            let
+                newTask =
+                    Task model.newInput True
+            in
+                { tasks = List.append model.tasks [ newTask ]
+                , newInput = ""
+                }
+
+        _ ->
+            model
 
 
 view : Model -> Html Msg
@@ -44,8 +65,9 @@ view model =
         [ ul []
             (List.map
                 showTask
-                model
+                model.tasks
             )
+        , newTask model
         ]
 
 
@@ -53,3 +75,16 @@ showTask : Task -> Html Msg
 showTask task =
     li []
         [ text task.task ]
+
+
+newTask : Model -> Html Msg
+newTask model =
+    div []
+        [ input
+            [ type_ "text"
+            , placeholder "Add Task"
+            , onInput TemporaryInput
+            ]
+            []
+        , button [ onClick Add ] [ text "Add" ]
+        ]
